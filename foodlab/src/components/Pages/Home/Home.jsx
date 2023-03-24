e0f5c72d73d980a551fcec1866c1d78fca039c6b
Tolea2001 <anatoliecurbat@gmail.com>
2023-03-24 18:55:52 +0200

    connect Our Specialties section

--- foodlab/src/components/Pages/Home/Home.jsx
+++ foodlab/src/components/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import CulinaryExperiencesSection from './Culinary Experiences Section/CulinaryExperiencesSection';
 import Slider from './Slider/Slider';
+import CulinaryExperiencesSection from './Culinary Experiences Section/CulinaryExperiencesSection';
+import OurSpecialties from './Our Specialties/OurSpecialties';
 
 export default function Home() {
   return (
     <>
       <Slider />
       <CulinaryExperiencesSection />
+      <OurSpecialties />
     </>
   )
 }
---
+++ foodlab/src/components/Pages/Home/Our Specialties/OurSpecialties.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import RotatedImage from '../../../Template/RotatedImage';
+import Image from 'react-bootstrap/Image';
+
+export default function OurSpecialties() {
+  return (
+    <div>OurSpecialties</div>
+  )
+}
