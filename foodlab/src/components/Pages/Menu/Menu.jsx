8da7d08ea4adf364f3e15ef3c984132f92f65867
Tolea2001 <anatoliecurbat@gmail.com>
2023-04-03 10:27:06 +0300

    commit

--- foodlab/src/components/Pages/Menu/Menu.jsx
+++ foodlab/src/components/Pages/Menu/Menu.jsx
@@ -1,8 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { BiSearch } from "react-icons/bi";
+import ProductCard from '../../Template/ProductCard';
 import PageHeader from '../../Template/PageHeader';
+import '../../../assets/styles/menu.css';
 
 export default function Menu() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/pizza')
+      .then(productsData => setProducts(productsData.data))
+  }, [])
+
   return (
-    <PageHeader textContent='Menu' />
+    <>
+      <PageHeader textContent='Menu' />
+      <Container>
+        <Row className='culinary-section w-100 mt6'>
+          <Col className='mb-4' sm={12} md={3}>
+            <ul className='text-center'>
+              <li>Pizza</li>
+              <li>Pasta</li>
+              <li>Meat</li>
+              <li>Salads</li>
+              <li>Soup</li>
+              <li>Beverages</li>
+            </ul>
+          </Col>
+          <Col sm={12} md={9}>
+            <Row>
+              <Form className="d-flex w-100">
+                <Form.Control
+                  type="text"
+                  placeholder="Search dishes"
+                  className='culinary-section-search'
+                  aria-label="Search"
+                />
+                <Button className='culinary-section-search-button'>
+                  <BiSearch className='fs-6' />
+                </Button>
+              </Form>
+            </Row>
+            <Row className='mt-5 ms-sm-0 ms-lg-2'>
+              {
+                products.map((product, index) => {
+                  return (
+                    <Col className='mb-4 culinary-section-product ' key={index}>
+                      <ProductCard
+                        cardTitle={product.productName}
+                        cardDescription={product.productDescription}
+                        productImage={product.productImage}
+                        price={product.initialPrice}
+                      />
+                    </Col>
+                  )
+                })
+              }
+            </Row>
+          </Col>
+        </Row>
+      </Container >
+    </>
+
+
   )
 }
--- foodlab/src/components/Template/ProductCard.jsx
+++ foodlab/src/components/Template/ProductCard.jsx
@@ -25,3 +25,4 @@ export default function productCard(props) {
     </Card>
   )
 }
+<p></p>
