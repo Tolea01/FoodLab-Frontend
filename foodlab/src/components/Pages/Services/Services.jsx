151d787a630e35caa433060b005c1e16b508940d
Tolea2001 <anatoliecurbat@gmail.com>
2023-04-11 19:52:02 +0300

    ServicesOfferedSection

--- foodlab/src/components/Pages/Services/Services.jsx
+++ foodlab/src/components/Pages/Services/Services.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PageHeader from '../../Template/PageHeader';
 import OverviewSection from './Overview Section/OverviewSection';
+import ServicesOfferedSection from './Services Offered Section/ServicesOfferedSection';
 
 export default function Services() {
   return (
     <>
       <PageHeader textContent='Services' />
       <OverviewSection />
+      <ServicesOfferedSection />
     </>
   )
 }
