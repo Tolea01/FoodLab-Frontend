52e60d5b0e162dabd23a1279d78503cbc87b9999
Tolea2001 <anatoliecurbat@gmail.com>
2023-04-13 00:42:03 +0300

    Map

--- foodlab/src/components/Pages/Contact us/Contact.jsx
+++ foodlab/src/components/Pages/Contact us/Contact.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PageHeader from '../../Template/PageHeader';
 import ContactDetails from './Contact Details/ContactDetails';
+import Map from './Map/Map';
 
 export default function Contact() {
   return (
     <>
       <PageHeader textContent='Contact Us' />
       <ContactDetails />
+      <Map />
     </>
   )
 }
