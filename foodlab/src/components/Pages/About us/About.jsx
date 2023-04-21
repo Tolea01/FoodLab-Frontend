7a17fb3b6c0411ea1d35485902354f9d24220fdd
Tolea2001 <anatoliecurbat@gmail.com>
2023-04-21 14:36:17 +0300

    add ShoppingCart Component in all pages

--- foodlab/src/components/Pages/About us/About.jsx
+++ foodlab/src/components/Pages/About us/About.jsx
@@ -3,14 +3,19 @@ import PageHeader from '../../Template/PageHeader';
 import MenuOptionsSection from './Menu Options  Section/MenuOptionsSection';
 import OurCompanySection from './Our Company Section/OurCompanySection';
 import PromotionalOfferSection from './Promotional Offer/PromotionalOfferSection';
+import { useShoppingCart } from '../../../hooks/useShoppingCart';
+import ShoppingCart from '../../Template/ShoppingCart';
 
 export default function About() {
+  const { productsInCart, setProductsInCart } = useShoppingCart();
+  
   return (
     <>
       <PageHeader textContent='About Us' />
       <MenuOptionsSection />
       <OurCompanySection />
       <PromotionalOfferSection />
+      <ShoppingCart products={productsInCart} setProducts={setProductsInCart} />
     </>
   )
 }
